c22009e0bd3c2604c97ad92ae0795f3e1805df8c
sojungvv <jungvv@gmail.com>
2025-07-27 18:00:55 +0900

    feat:컴포넌트 구현

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,4 +1,5 @@
 import Dah from "./Dah";
+import Soj from "./Soj";
 import Sub from "./Sub";
 
 export default function Body(props) {
@@ -7,6 +8,7 @@ export default function Body(props) {
       <div className="h-auto flex flex-row w-screen flex-grow">
         <Dah />
         <Sub />
+        <Soj></Soj>
       </div>
     </>
   );
---
+++ src/components/Soj.jsx
@@ -0,0 +1,9 @@
+export default function Soj(){
+    return (
+        <>
+        <div className="bg-pink-200 w-1/2 flex gap-2 flex-col justify-center items-center">
+            <div> 로코대박</div>
+        </div>
+        </>
+    )
+}
